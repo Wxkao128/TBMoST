d3724aaf348a1f5377ba4463f41fda384117ab98
Wxkao128 <96676645+Wxkao128@users.noreply.github.com>
2025-02-16 22:04:40 +0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -7,11 +7,14 @@ release = '1.0'
 master_doc = 'TBMoST_docs_tc'  # (index.rst or index.md)
 
 extensions = [
-    'sphinx.ext.imgmath',
-    'sphinx.ext.mathjax',
+    'sphinx.ext.mathjax',  # 保留 MathJax
     'sphinx.ext.autodoc',
     'sphinx.ext.napoleon',
 ]
+
+latex_elements = {
+    'preamble': r'\usepackage{braket}',  # 加載 braket 宏包
+}
 
 html_theme_options = {
     'mathjax_path': 'https://cdn.jsdelivr.net/npm/mathjax@2/es5/tex-mml-chtml.js',
@@ -20,11 +23,8 @@ html_theme_options = {
 mathjax_config = {
     "TeX": {
         "Macros": {
-            "bra": r"\\langle",
-            "ket": r"\\rangle",
+            "bra": r"\langle",  # 定義 bra 符號
+            "ket": r"\rangle",  # 定義 ket 符號
         }
     }
 }
-
-
-#language = 'zh'
