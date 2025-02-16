a3f5a95ce08f67451ae3d48b518d8c4727cb0c98
Wxkao128 <96676645+Wxkao128@users.noreply.github.com>
2025-02-16 17:35:39 +0800

    Add files via upload

---
+++ docs/conf.py
@@ -0,0 +1,7 @@
+# -- Basic Info -----------------------------------------------------
+project = 'TBMoST'
+author = 'wxkao128'
+release = '1.0'
+
+# -- Main -----------------------------------------------------------
+master_doc = 'TBMoST.md'  # (index.rst or index.md)
